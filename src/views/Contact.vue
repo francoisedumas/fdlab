056d6e86b17966fc45fb381b79f5ec07317fe0c6
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-10-18 22:05:16 +0200

    New redirection test

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,6 +6,7 @@
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      action="/thanks"
     >
       <input type="hidden" name="form-name" value="contactForm" />
       <div>
