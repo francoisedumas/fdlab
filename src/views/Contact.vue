1e5ea96a8888f8fb37ba59992e5fc03fa6cde1a0
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-10-18 22:00:45 +0200

    Roll back redirection

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,7 +6,6 @@
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      action="/thanks"
     >
       <input type="hidden" name="form-name" value="contactForm" />
       <div>
