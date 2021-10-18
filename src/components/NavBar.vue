0439a5d22433fd0fc31bb74ab788b7311bc21fb3
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-10-18 22:26:15 +0200

    Link to thanks

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,6 +6,7 @@
       <router-link :to="{ name: 'Projects' }">Projects</router-link>
       <router-link :to="{ name: 'Til' }">TIL</router-link>
       <router-link :to="{ name: 'Contact' }">Contact</router-link>
+      <router-link :to="{ name: 'Thanks' }">Thanks</router-link>
     </nav>
   </header>
 </template>
