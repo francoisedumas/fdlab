3f6c43c407c90a62166105f01943d38bca1d5cce
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-11-15 23:17:05 +0100

    Navbar new format simplified

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,17 @@
 <template>
-  <header>
-    <router-link :to="{ name: 'Home' }" class="nav-title">
-      <h1>FDL</h1>
-    </router-link>
-    <nav class="nav-links">
-      <router-link :to="{ name: 'Home' }">Home</router-link>
-      <router-link :to="{ name: 'Projects' }">Projects</router-link>
-      <router-link :to="{ name: 'Til' }">TIL</router-link>
-      <router-link :to="{ name: 'Contact' }">Contact</router-link>
+  <div class="navbar">
+    <nav>
+      <router-link :to="{ name: 'Home' }" class="nav-title">
+        <h1>FDL</h1>
+      </router-link>
+      <div class="links">
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link :to="{ name: 'Projects' }">Projects</router-link>
+        <router-link :to="{ name: 'Til' }">TIL</router-link>
+        <router-link :to="{ name: 'Contact' }">Contact</router-link>
+      </div>
     </nav>
-  </header>
+  </div>
 </template>
 
 <script>
@@ -17,46 +19,42 @@ export default {};
 </script>
 
 <style scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  max-width: 1200px;
+.navbar {
   margin: 0 auto;
   padding: 20px 0px;
   border-bottom: 1px dashed black;
   color: var(--light-gray);
 }
-.nav-links {
-  margin-right: 20px;
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
 }
-.nav-links a {
+nav .links {
+  margin-left: auto;
   padding-right: 25px;
 }
-.nav-title {
-  flex: 1;
-}
-header h1 {
+nav h1 {
   font-size: 32px;
   margin-left: 20px;
   color: var(--light-gray);
 }
-header a.router-link-active {
+nav .links a {
+  margin-left: 16px;
+}
+.navbar a.router-link-active {
   color: var(--primary);
   font-weight: bold;
 }
 
 @media (max-width: 640px) {
-  header h1 {
+  .navbar h1 {
     font-size: 24px;
     margin-left: 5px;
   }
-  .nav-links a {
+  nav .links {
     padding-right: 10px;
-  }
-  .nav-links {
-    margin-right: 10px;
   }
 }
 </style>
